0571aa0e2997f6f932b67ecccbd6d35edb25cd40
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-08-26 17:07:28 +0200

    M3

--- script.js
+++ script.js
@@ -76,8 +76,10 @@ function startGame(e) {
     const maxScore = totalCells - totalBombs;
 
     //# Processing
-    // Genero le celle
 
+    // Genero le bombe
+
+    // Ottengo 16 numeri casuali e li stampo in console
     const bombs = getRandomNumber(totalCells, totalBombs);
     console.log(bombs);
 
@@ -91,13 +93,21 @@ function startGame(e) {
         cell.addEventListener('click', function () {
             console.log(parseInt(cell.innerText));
 
-            getRandomNumber();
-            console.log(randomNumbers);
-
+            // Se la cella è già stata cliccata, blocco il suo comportamento
             if (cell.classList.contains('clicked')) return;
 
-            cell.classList.add('clicked');
-            scorePoints.innerText = ++score;
+            // Confronto se il numero della cella è nell'array delle bombe
+            if (bombs.includes(i + 1)) {
+                cell.classList.add('bomb');
+                console.log('Bomba');
+                console.log(`Hai perso. Il tuo punteggio è: ${score}`)
+            } else {
+                // Aggiungo la classe clicked alla cella
+                cell.classList.add('clicked');
+                // Incremento il punteggio
+                scorePoints.innerText = ++score;
+            }
+
         });
 
         grid.appendChild(cell);
@@ -126,24 +136,25 @@ Se riusciamo, facciamo anche in modo da non poter più cliccare sulla stessa cel
 - Al click sulla cella, cambio il valore dello score (Aumento di 1)
 - Blocco la funzione sulla cella una volta cliccata creando un flag usando la classe
 
-
 # MILESTONE 2
 Facciamo in modo di generare 16 numeri casuali (tutti diversi) compresi tra 1 e il massimo di caselle disponibili.
 Generiamoli e stampiamo in console per essere certi che siano corretti
-
 
 - creo una funzione per generare 16 numeri random (tra 1 e totalCells)
 - Faccio in modo che siano diversi
 - Console log
 
-
-
 # MILESTONE 3
 Quando l'utente clicca su una cella, verifichiamo se ha calpestato una bomba, controllando se il numero di cella è presente nell'array di bombe.
-Se si, la cella diventa rossa (raccogliamo il punteggio e scriviamo in console che la patita termina) altrimenti diventa azzurra e dobbiamo incrementare il punteggio.
+Se si, la cella diventa rossa (raccogliamo il punteggio e scriviamo in console che la partita termina) altrimenti diventa azzurra e dobbiamo incrementare il punteggio.
+
+- Sull'evento click, vedo se il numero cliccato è nell'array Bombs con un if
+- Aggiungo la classe .bomb alla cella (per aggiungere un bcg red)
+- Console.log ("Hai perso + punteggio")
+
 # MILESTONE 4
 Quando l'utente clicca su una cella, e questa non è una bomba, dobbiamo controllare se il punteggio incrementato ha raggiunto il punteggio massimo, perchè in quel caso la partita termina. Raccogliamo quindi il punteggio e scriviamo un messaggio appropriato.
 # MILESTONE 5
-Quando la partita termina dobbiamo capire se è terminata perchè è stata cliccata una bomba o seperchè l'utente ha raggiunto il punteggio massimo(ossia ha vinto). Dobbiamo poi in ogni caso stampare lin pagina il punteggio raggiunto ed il messaggio adeguato in caso di vittoria o sconfitta.
+Quando la partita termina dobbiamo capire se è terminata perchè è stata cliccata una bomba o se perchè l'utente ha raggiunto il punteggio massimo(ossia ha vinto). Dobbiamo poi in ogni caso stampare lin pagina il punteggio raggiunto ed il messaggio adeguato in caso di vittoria o sconfitta.
 */
 
